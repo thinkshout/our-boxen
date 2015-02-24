0c3418725e9ebfcf48b6f228b32bfd156fa27836
Lev Tsypin <lev@thinkshout.com>
2015-02-24 09:37:52 -0800

    Only include latest versions of Ruby and node.

--- manifests/site.pp
+++ manifests/site.pp
@@ -65,16 +65,10 @@ node default {
   }
 
   # node versions
-  nodejs::version { 'v0.6': }
-  nodejs::version { 'v0.8': }
-  nodejs::version { 'v0.10': }
+  nodejs::version { 'v0.12.0': }
 
   # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
-  ruby::version { '2.1.2': }
+  ruby::version { '2.2.0': }
 
   # common, useful packages
   package {
